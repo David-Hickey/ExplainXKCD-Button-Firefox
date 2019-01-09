584237439264165b04c265b6538d6b8ba047060b
David Hickey <david.hickey15@imperial.ac.uk>
2019-01-09 02:06:24 +0000

    Added settings

--- explainbutton.js
+++ explainbutton.js
@@ -4,20 +4,26 @@ let extractNumber = /^https?:\/\/(?:www\.)?xkcd.com\/(?:(\d*)\/?)?$/i;
 let match = extractNumber.exec(window.location);
 
 if (match) {
-  // Select the fourth button in the top-left div, which is the
-  // Store button.
-  let storeButton = document.querySelectorAll("div#topLeft li")[3];
-  let buttonHref = storeButton.firstChild;
+  // Load the option to replace, then replace it.
+  browser.storage.local.get('replaced_button').then((res) => {
+    let replaceIndex = parseInt(res.replaced_button);
 
-  // Extract the comic number from the match. If no number in the URL
-  // then this will be falsy, so we can easily handle that case.
-  let comicNumber = match[1];
+    // Select the fourth button in the top-left div, which is the
+    // Store button.
+    let storeButton = document.querySelectorAll("div#topLeft li")[replaceIndex];
+    let buttonHref = storeButton.firstChild;
 
-  if (comicNumber) {
-    buttonHref.href = "https://explainxkcd.com/" + comicNumber + "/";
-  } else {
-    buttonHref.href = "https://explainxkcd.com/";
-  }
+    // Extract the comic number from the match. If no number in the URL
+    // then this will be falsy, so we can easily handle that case.
+    let comicNumber = match[1];
 
-  buttonHref.text = "Explain";
+    if (comicNumber) {
+      buttonHref.href = "https://explainxkcd.com/" + comicNumber + "/";
+    } else {
+      buttonHref.href = "https://explainxkcd.com/";
+    }
+
+    buttonHref.text = "Explain";
+
+  });
 }
---
+++ options.js
@@ -0,0 +1,41 @@
+
+function saveOptions(e) {
+  browser.storage.local.set({
+    "replaced_button": getSelectedRadioValue()
+  });
+  e.preventDefault();
+}
+
+
+function getSelectedRadioValue() {
+  let radios = document.getElementsByName("replaced_button_opt");
+
+  for (let i = 0; i < radios.length; ++i) {
+    if (radios[i].checked) {
+      return radios[i].value;
+    }
+  }
+
+  throw "this should never happen";
+}
+
+
+function setSelectedRadioValue(v) {
+  let radios = document.getElementsByName("replaced_button_opt");
+
+  for (let i = 0; i < radios.length; ++i) {
+    radios[i].checked = (radios[i].value == v);
+  }
+}
+
+
+function restoreOptions() {
+  var storageItem = browser.storage.local.get('replaced_button');
+  storageItem.then((res) => {
+    setSelectedRadioValue(res.replaced_button);
+  });
+}
+
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
